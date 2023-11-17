cb3b2478f63c6486221fafaafb1e1282be88b774
Kenny Phongsavan <kennyphongsavan@cyanacklabs.dev>
2023-11-16 20:34:45 -0500

    update 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 // Parse text file into byte stream
-use std::fs:File;
-use std::io::Read;
+use std::fs::File;
+use std::io::{self, Read};
 
-fn main() -> std::io:Result<()> {
+fn main() -> io::Result<()> {
     let file_path = "input.txt";
     let mut file = File::open(file_path)?;
     let mut content = String::new();    // For now, testing with string buffer
+
+    file.read_to_string(&mut content)?; // Read file into buffer
+
+    println!("File content: {}", content);
+
+    Ok(())
 }
