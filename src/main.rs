8ef306bbb938631e35aab9facfb9d845d00aad83
Kenny Phongsavan <kennyphongsavan@cyanacklabs.dev>
2023-11-16 22:52:09 -0500

    updated to output binary and strings

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,36 @@
 use std::fs::File;
 use std::io::{self, Read};
 
+// Read file into a vector of bytes
+fn read_file_vec8(file_path: &str) -> io::Result<Vec<u8>> {
+    let mut file = File::open(file_path)?;
+    let mut content = Vec::new();
+
+    file.read_to_end(&mut content)?;
+
+    Ok(content)
+}
+
+// Read file into a string
+fn read_file_str(file_path: &str) -> io::Result<String> {
+    let mut file = File::open(file_path)?;
+    let mut content = String::new();
+
+    file.read_to_string(&mut content)?;
+
+    Ok(content)
+}
 fn main() -> io::Result<()> {
     let file_path = "input.txt";
-    let mut file = File::open(file_path)?;
-    let mut content = String::new();    // For now, testing with string buffer
+    
+    let content_vec8 = read_file_vec8(file_path)?;
+    let content_str = read_file_str(file_path)?;
 
-    file.read_to_string(&mut content)?; // Read file into buffer
+    for byte in &content_vec8 {
+        print!("{:02X} ", byte);
+    }
 
-    println!("File content: {}", content);
+    println!("\nText content: {}", content_str);
 
     Ok(())
 }
